4a4bb8d4f22d9b820927bcb5d3f1cbdec354db1b
enamtoet <otmane.ettalabi1@gmail.com>
2023-11-04 14:40:05 +0100

    Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,13 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    screens:{
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
-    },
-      
     extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
       colors: {
         brightRed:'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
